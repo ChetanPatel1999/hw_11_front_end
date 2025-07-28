fbd21bde7c063bc008af0cb6a36b7959f19b7d4e
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-07-28 12:11:03 +0530

    26/07

---
+++ currency_converter/codes.js
@@ -0,0 +1,27 @@
+const currencyToCountry = {
+  USD: "US",     // United States Dollar -> United States
+  EUR: "DE",     // Euro -> Germany (EU countries use EUR, using DE as representative)
+  INR: "IN",     // Indian Rupee -> India
+  GBP: "GB",     // British Pound -> United Kingdom
+  JPY: "JP",     // Japanese Yen -> Japan
+  AUD: "AU",     // Australian Dollar -> Australia
+  CAD: "CA",     // Canadian Dollar -> Canada
+  CNY: "CN",     // Chinese Yuan -> China
+  CHF: "CH",     // Swiss Franc -> Switzerland
+  RUB: "RU",     // Russian Ruble -> Russia
+  BRL: "BR",     // Brazilian Real -> Brazil
+  ZAR: "ZA",     // South African Rand -> South Africa
+  NZD: "NZ",     // New Zealand Dollar -> New Zealand
+  KRW: "KR",     // South Korean Won -> South Korea
+  MXN: "MX",     // Mexican Peso -> Mexico
+  SGD: "SG",     // Singapore Dollar -> Singapore
+  SEK: "SE",     // Swedish Krona -> Sweden
+  NOK: "NO",     // Norwegian Krone -> Norway
+  TRY: "TR",     // Turkish Lira -> Turkey
+  SAR: "SA",     // Saudi Riyal -> Saudi Arabia
+  AED: "AE",     // UAE Dirham -> United Arab Emirates
+  HKD: "HK",     // Hong Kong Dollar -> Hong Kong
+  THB: "TH",     // Thai Baht -> Thailand
+  MYR: "MY",     // Malaysian Ringgit -> Malaysia
+  IDR: "ID",     // Indonesian Rupiah -> Indonesia
+};
