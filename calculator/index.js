f04e704065659a233b2b39b7be290a84131fb6e7
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-08-04 12:06:59 +0530

    04/08

---
+++ calculator/index.js
@@ -0,0 +1,26 @@
+let input = document.querySelector("#input-box")
+let buttons = document.querySelectorAll("button")
+
+let string = ""
+buttons.forEach(button => {
+    button.addEventListener("click", evt => {
+        let val = evt.target.innerHTML;
+        console.log(val)
+        if (val == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if (val == 'AC') {
+            string = ""
+            input.value = string
+        }
+        else if (val == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string
+        }
+        else {
+            string = string + val;
+            input.value = string;
+        }
+    })
+})
