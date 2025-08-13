2f8af19ee311541360ea29c4c583050409665995
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-08-13 21:29:32 +0530

    08/13

--- Quiz app/index.js
+++ Quiz app/index.js
@@ -1,69 +1,113 @@
 const questions = [
     {
-        question: "Which of the following is used to store multiple values in a single variable in most programming languages?",
+        question: "Which of the following is used to store multiple values in a single variable in JavaScript?",
         answers: [
-            { text: "Function", correct: false },
+            { text: "String", correct: false },
             { text: "Array", correct: true },
-            { text: "Loop", correct: false },
-            { text: "Operator", correct: false },
+            { text: "Number", correct: false },
+            { text: "Boolean", correct: false },
         ]
     },
     {
-        question: "What is the correct symbol for “equal to” in most programming languages?",
+        question: "What does HTML stand for?",
         answers: [
-            { text: "=", correct: false },
-            { text: "==", correct: true },
-            { text: "===", correct: false },
-            { text: ":=", correct: false },
+            { text: "HyperText Markup Language", correct: true },
+            { text: "HighText Machine Language", correct: false },
+            { text: "HyperTool Markup Language", correct: false },
+            { text: "HighText Mark Language", correct: false },
         ]
     },
     {
-        question: "In programming, which loop executes at least once even if the condition is false?",
+        question: "In programming, which symbol is commonly used for single-line comments in JavaScript?",
         answers: [
-            { text: "for", correct: false },
-            { text: "while", correct: false },
-            { text: "do-while", correct: true },
-            { text: "forEach", correct: false },
+            { text: "<!---", correct: false },
+            { text: "#", correct: false },
+            { text: "//", correct: true },
+            { text: "/#", correct: false },
         ]
     },
     {
-        question: "Which of these is NOT a programming language?",
+        question: "In CSS, which property is used to change the text color?",
         answers: [
-            { text: "HTML", correct: true },
-            { text: "python", correct: false },
-            { text: "java", correct: false },
-            { text: "C++", correct: false },
+            { text: "background-color", correct: false },
+            { text: "text-align", correct: false },
+            { text: "color", correct: true },
+            { text: "font-size", correct: false },
         ]
-    },
+    }
 ]
 
-let questionIndex = 0;
+const questionElement = document.querySelector("#question");
+const buttonsElement = document.querySelector(".answer-buttons");
+const nextBtn = document.querySelector("#next-btn");
+
+let currentQuestionIndex = 0;
 let score = 0;
 
-const questionElement = document.querySelector("#question");
-const buttonsElement = document.querySelector(".answer-buttons")
-const nextBtn = document.querySelector("#next-btn")
-
-function quizStart() {
-    resetbtn();
-    questionIndex = 0;
+function startQuiz() {
+    nextBtn.innerHTML = "Next";
+    currentQuestionIndex = 0;
     score = 0;
-    showQuestions();
+    showQuestion();
 }
-function showQuestions() {
-    let questionData = questions[questionIndex];
-    let currentQuestionIndex = questionIndex + 1;
-    questionElement.innerHTML = `${currentQuestionIndex}. ${questionData.question}`;
+function showQuestion() {
+    resetButton();
+    let questionData = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + questionData.question;
+
     questionData.answers.forEach(answer => {
         let button = document.createElement("button");
-        button.classList.add("btn");
         button.innerText = answer.text;
+        button.classList.add("btn");
         buttonsElement.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAns);
     })
 }
-function resetbtn() {
+function selectAns(evt) {
+    let ans = evt.target.dataset.correct;
+    if (ans) {
+        evt.target.classList.add("correct-option");
+        score++;
+    }
+    else {
+        evt.target.classList.add("incorrect-option");
+    }
+    Array.from(buttonsElement.children).forEach(button => {
+        if (button.dataset.correct) {
+            button.classList.add("correct-option");
+        }
+        button.disabled = true;
+    })
+    nextBtn.style.display = "block";
+}
+function resetButton() {
+    nextBtn.style.display = "none";
     while (buttonsElement.firstChild) {
         buttonsElement.firstChild.remove();
     }
 }
-quizStart();
+function showQuizeScore() {
+    if (currentQuestionIndex < questions.length + 1) {
+        resetButton();
+        questionElement.innerHTML = `Quize score ${score} outof ${questions.length}`;
+        nextBtn.style.display = "block";
+        nextBtn.innerHTML = "Play Again";
+    }
+    else {
+        startQuiz();
+    }
+}
+nextBtn.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    }
+    else {
+        showQuizeScore();
+    }
+})
+startQuiz();
